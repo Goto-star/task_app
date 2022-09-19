c46c171dd50f2fd07de35211d717eef22a5dd751
Goto-star <100769306+Goto-star@users.noreply.github.com>
2022-09-19 12:31:30 +0900

    Revert "finish rails_task 01"

--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-    validates :title, presence: { message: "を入力してください" }
-    validates :start_day, presence: { message: "を入力してください" }
-    validates :posted_day, presence: { message: "を入力してください" }
-end
